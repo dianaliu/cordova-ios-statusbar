48eec7d05d4625cca8265cc4d3dbe74444c3d00a
dianaliu <diana.g.liu@gmail.com>
2013-11-17 15:00:07 -0500

    Update js style

--- www/StatusBar.js
+++ www/StatusBar.js
@@ -6,27 +6,24 @@
 // Copyright 2013 Parick Heneise. All rights reserved.
 // MIT Licensed
 
-(function(cordova) {
-	function StatusBar (){};
- 
-  StatusBar.prototype.show = function(callback) {
-    cordova.exec(callback, null, 'StatusBar', 'show', []);
-  };
- 
-  StatusBar.prototype.hide = function(callback) {
-    cordova.exec(callback, null, 'StatusBar', 'hide', []);
-  };
- 
-  StatusBar.prototype.whiteTint = function(callback) {
-    cordova.exec(callback, null, 'StatusBar', 'whiteTint', []);
-  };
- 
-  StatusBar.prototype.blackTint = function(callback) {
-    cordova.exec(callback, null, 'StatusBar', 'blackTint', []);
+var exec = require('cordova/exec');
+
+var StatusBar = function StatusBar() {
+  this.show = function(callback) {
+    exec(callback, null, 'StatusBar', 'show', []);
   };
 
-  cordova.addConstructor(function() {
-    if(!window.plugins) window.plugins = {};
-    window.plugins.statusBar = new StatusBar();
-  });
- })(window.cordova || window.Cordova);
+  this.hide = function(callback) {
+    exec(callback, null, 'StatusBar', 'hide', []);
+  };
+
+  this.whiteTint = function(callback) {
+    exec(callback, null, 'StatusBar', 'whiteTint', []);
+  };
+
+  this.blackTint = function(callback) {
+    exec(callback, null, 'StatusBar', 'blackTint', []);
+  };
+};
+
+module.exports = new StatusBar();
